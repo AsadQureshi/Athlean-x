beed0142c3a6de57542200440c6b56492120986a
AsadQureshi <asad.quereshi@metikulous.com>
2014-06-16 15:23:09 +0500

    all modules and views done

--- routes/workout.js
+++ routes/workout.js
@@ -1,37 +1,13 @@
 var workout = require('../models/Workout.js');
-var exercise = require('../models/Exercise.js');
-
 exports.viewWorkout = function(req, res) {
     res.render('workout.html');
 }
-// exports.addworkout = function(req, res) {
-
-//     if (req.body) {
-//         var data = new workoutSchema({
-//             date: req.body.date,
-//             workouts: req.body.Exercise,
-//             Raps: req.body.Raps,
-//         });
-//         data.save(function(err, data) {
-//             if (err) {
-//                 res.send(err);
-//             } else {
-//                 console.log(data);
-//                 res.send(data);
-
-//             }
-//         });
-//     } else {
-//         console.log('body is empty');
-//     }
-// }
-
-exports.testnestedSchema = function(req, res) {
+exports.nestedSchema = function(req, res) {
     var wk = new workout();
 
     wk.exercises.push({
-        exercise: 'Squats',
-        reps: 10
+        exercise: req.body.Exercise,
+        reps: req.body.reps,
     });
 
     wk.save(function(err, doc) {
@@ -39,6 +15,7 @@ exports.testnestedSchema = function(req, res) {
             console.log(err)
         else
             console.log(doc);
+            res.send(doc);
     });
 
 };
