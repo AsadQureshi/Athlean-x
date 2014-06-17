dd89635d17f284e8728a107659456e72337c6674
Sultan Saadat <sultansaadat@gmail.com>
2014-06-17 12:52:00 +0500

    create a new user and verify an existing user

--- routes/login.js
+++ routes/login.js
@@ -12,7 +12,7 @@ exports.getLogin = function(req, res) {
     // login.find({
 
     // }, function(err, doc) {
-        
+
     // })
 
     // if (login.password === req.body.password) {
@@ -23,4 +23,61 @@ exports.getLogin = function(req, res) {
     //     res.send("try again")
     // }
 
+
+    //create a new user with the new schema
+    
+    var user = new User();
+    user.username = 'sultansaadat';
+    user.email = 'sultansaadat@gmail.com';
+    user.password = user.generateHash('test123');
+    // user.sessionToken = null;
+
+
 };
+
+
+// verify existing user
+
+// exports.login = function(req, res) {
+//     User.findOne({
+//         username: 'sultansaadat'
+//     }, function(err, user) {
+//         if (err) {
+//             res.json(err);
+//         };
+//         if (!user) {
+//             res.json('User Not Found');
+//         };
+//         if (user.validatePassword('test123')) {
+//             if (!user.sessionToken) {
+//                 user.sessionToken = generateToken();
+//                 user.save(function(err) {
+//                     if (err) {
+//                         res.json(err);
+//                     } else {
+//                         res.send(200, {
+//                             sessionkey: user.sessionToken,
+//                             status: 'success',
+//                             message: 'logged in successfully',
+//                             code: 200
+//                         });
+//                     }
+//                 });
+//             } else {
+//                 res.send(200, {
+//                     sessionkey: user.sessionToken,
+//                     status: 'success',
+//                     message: 'already logged in',
+//                     code: 200
+//                 });
+//             }
+//         } else {
+//             res.send(200, {
+//                 status: 'failure',
+//                 message: 'invalid password',
+//                 code: 200
+//             });
+//         }
+
+//     });
+// };
